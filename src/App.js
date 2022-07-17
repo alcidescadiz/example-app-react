de7ab6c0177fb2ecd3b41dea9ba01b86918e4eab
alcidescadiz <alcides.cadiz@gmail.com>
2022-07-17 11:25:07 -0400

    add loading

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
-import { Routes, Route, HashRouter} from 'react-router-dom'
-import Conmemoracion from './components/commemoracion.js';
-import MenuApp from './components/layaut/index.js'
-import Pie from './components/layaut/pie.js';
-import Lectura from './components/lectura.js';
-import LecturaConme from './components/LecturaConme.js';
-import Libros from './components/libros.js';
-import Relatos from './components/relatos.js';
-import Videos from './components/videos.js';
-import  {CheckedContextComponent} from './components/checkedContex/index'
-
+import React, { lazy, Suspense } from "react";
+import { Routes, Route, HashRouter } from "react-router-dom";
+import Conmemoracion from "./components/commemoracion.js";
+import MenuApp from "./components/layaut/index.js";
+import Pie from "./components/layaut/pie.js";
+import Lectura from "./components/lectura.js";
+import LecturaConme from "./components/LecturaConme.js";
+import Relatos from "./components/relatos.js";
+import Videos from "./components/videos.js";
+import LoadingInicial from "./components/loading";
+import { CheckedContextComponent } from "./components/checkedContex/index";
+const Libros = lazy(() => import("./components/libros.js"));
 
 function App() {
   return (
-      <HashRouter>
-            <CheckedContextComponent>
-              <MenuApp />
-              <Routes>
-                  <Route  path='/' >
-                    <Route index element={<Libros  />} />
-                    <Route path='lectura/:id/:idl/:inicio/:fin' element={<Lectura />} />
-                    <Route path='relatos/:id/:inicio/:fin' element={<Relatos />} />
-                    <Route path='videos/:id/:idl/:inicio/:fin' element={<Videos />} />
-                  </Route>
-                  <Route path='/conmemoracion' >
-                    <Route index element={<Conmemoracion />} />
-                    <Route path='lecturaconme/:id' element={<LecturaConme />} />
-                  </Route> 
-                  <Route path='*' element={<Libros  />} />
-              </Routes>
-              <Pie />
-            </CheckedContextComponent>
-      </HashRouter>
+    <HashRouter>
+      <Suspense fallback={<LoadingInicial />}>
+        <CheckedContextComponent>
+          <MenuApp />
+          <Routes>
+            <Route path="/">
+              <Route index element={<Libros />} />
+              <Route
+                path="lectura/:id/:idl/:inicio/:fin"
+                element={<Lectura />}
+              />
+              <Route path="relatos/:id/:inicio/:fin" element={<Relatos />} />
+              <Route path="videos/:id/:idl/:inicio/:fin" element={<Videos />} />
+            </Route>
+            <Route path="/conmemoracion">
+              <Route index element={<Conmemoracion />} />
+              <Route path="lecturaconme/:id" element={<LecturaConme />} />
+            </Route>
+            <Route path="*" element={<Libros />} />
+          </Routes>
+          <Pie />
+        </CheckedContextComponent>
+      </Suspense>
+    </HashRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-import React, {lazy, Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import LoadingInicial from './components/loading';
-const App = lazy(()=> import('./App'));
-
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Suspense fallback={<LoadingInicial/>} >
         <App />
-    </Suspense>
 );
 
 serviceWorkerRegistration.register();
