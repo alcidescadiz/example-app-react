4d58a6437cd844d5ac309471d2347f80e4de5f20
alcidescadiz <alcides.cadiz@gmail.com>
2022-07-17 09:28:37 -0400

    lazy mode and loading

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import LoadingInicial from './components/loading';
+const App = lazy(()=> import('./App'));
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Suspense fallback={<LoadingInicial/>} >
+        <App />
+    </Suspense>
 );
 
 serviceWorkerRegistration.register();
